b4eb7cb39545bcd024d3f309c26ace514b1856b3
impust531 <impust.f@gmail.com>
2025-04-20 21:44:12 +0900

    Add CORS header

--- api/quote.js
+++ api/quote.js
@@ -3,6 +3,8 @@ import { Client } from '@notionhq/client';
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
   try {
     const response = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID,
